66f7c2b30c3e1d1ff55973884904e8ce7bd0bebd
waal70 <waal70@hotmail.com>
2024-01-03 18:57:19 +0100

    Fix log2ram deprecation of apt-key

--- firstrun.sh
+++ firstrun.sh
@@ -2,6 +2,8 @@
 export PRIVATE_REPO=../home
 export SSH_ASKPASS=${PRIVATE_REPO}/ssh-keys/passphrase
 export SSH_ASKPASS_REQUIRE=force
+# Andre fix 01/2024: from git other properties are set
+chmod 0600 ${PRIVATE_REPO}/ssh-keys/ansible-key
 ssh-agent bash
 ssh-add ${PRIVATE_REPO}/ssh-keys/ansible-key
 
