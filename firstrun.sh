4275aebf8845972db7d68efa98c4ee842e51d0b0
waal70 <waal70@hotmail.com>
2024-01-08 12:14:49 +0100

    Try commit vscode

--- firstrun.sh
+++ firstrun.sh
@@ -2,6 +2,7 @@
 export PRIVATE_REPO=../home
 export SSH_ASKPASS=${PRIVATE_REPO}/ssh-keys/ansible/passphrase
 export SSH_ASKPASS_REQUIRE=force
+# TODO: find a better option for this
 ssh-agent bash
 ssh-add ${PRIVATE_REPO}/ssh-keys/ansible/ansible-key
 ssh-add ${PRIVATE_REPO}/ssh-keys/awaal/awaal-key
