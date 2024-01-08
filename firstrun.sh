081db102ad7d171f4b00a23ee7aef6cbb1926456
waal70 <waal70@hotmail.com>
2024-01-08 12:12:12 +0100

    Test commit

--- firstrun.sh
+++ firstrun.sh
@@ -4,3 +4,4 @@ export SSH_ASKPASS=${PRIVATE_REPO}/ssh-keys/ansible/passphrase
 export SSH_ASKPASS_REQUIRE=force
 ssh-agent bash
 ssh-add ${PRIVATE_REPO}/ssh-keys/ansible/ansible-key
+ssh-add ${PRIVATE_REPO}/ssh-keys/awaal/awaal-key
