b9f67ffc790bbb9cfcbaf3088a80311ed6343b58
waal70 <waal70@hotmail.com>
2024-01-08 12:04:33 +0100

    Make tftp - netboot a role

--- firstrun.sh
+++ firstrun.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export PRIVATE_REPO=../home
-export SSH_ASKPASS=${PRIVATE_REPO}/ssh-keys/passphrase
+export SSH_ASKPASS=${PRIVATE_REPO}/ssh-keys/ansible/passphrase
 export SSH_ASKPASS_REQUIRE=force
-# Andre fix 01/2024: from git other properties are set
-chmod 0600 ${PRIVATE_REPO}/ssh-keys/ansible-key
 ssh-agent bash
-ssh-add ${PRIVATE_REPO}/ssh-keys/ansible-key
+ssh-add ${PRIVATE_REPO}/ssh-keys/ansible/ansible-key
