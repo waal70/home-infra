2b9f2297cff233007ad8593a3d7fb6802f46b0f0
waal70 <waal70@hotmail.com>
2024-11-01 18:11:29 +0100

    Add role backup Active Directory Add check for age of backup Change firstrun.sh and ansible.cfg to handle PRIVATE_REPO env variable

--- firstrun.sh
+++ firstrun.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-export PRIVATE_REPO=../home
+
+export PRIVATE_REPO=${HOME}/ansible/home
 export SSH_ASKPASS=${PRIVATE_REPO}/ssh-keys/ansible/passphrase
 export SSH_ASKPASS_REQUIRE=force
-# TODO: find a better option for this
+
 ssh-agent bash
 ssh-add ${PRIVATE_REPO}/ssh-keys/ansible/ansible-key
 ssh-add ${PRIVATE_REPO}/ssh-keys/awaal/awaal-key
