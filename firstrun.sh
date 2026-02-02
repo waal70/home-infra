426c94c517758c04aa6441a1863cfdfc9532a658
waal70 <myemail@myprovider.lol>
2026-02-02 20:03:02 +0100

    fix: swapping ssh key needed fixing

--- firstrun.sh
+++ firstrun.sh
@@ -7,4 +7,4 @@ if [ ! -d "${HOME}/.ansible/roles/waal70.debian_common" ]; then
   ansible-galaxy install -g -f -r roles/requirements.yml
 fi
 
-./sign_ssh_key.sh -c ${PRIVATE_REPO}/ssh-keys/ca/ca-ed25519 -i ansible -p ansible -v "+10m"
+echo y | ./sign_ssh_key.sh -c ${PRIVATE_REPO}/ssh-keys/ca/ca-ed25519 -i ansible -p ansible -v "+30m"
