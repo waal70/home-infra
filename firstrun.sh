0718774ec5377543423987fc78066c6d87159ef5
waal70 <myemail@myprovider.lol>
2026-02-05 23:06:34 +0100

    fix: move to daily certs for ansible

--- firstrun.sh
+++ firstrun.sh
@@ -10,4 +10,4 @@ fi
 # For Yubikey-backed SSH keys:
 # echo y | ./sign_ssh_key.sh -c ${PRIVATE_REPO}/ssh-keys/ca/ca.pub -i ansible -p ansible -v "+30m"
 # For regular SSH keys:
-echo y | ./sign_ssh_key.sh -c ${PRIVATE_REPO}/ssh-keys/ca/ca-ed25519 -i ansible -p ansible -v "+30m"
+echo y | ./sign_ssh_key.sh -c ${PRIVATE_REPO}/ssh-keys/ca/ca-ed25519 -i ansible -p ansible -v "+12h"
