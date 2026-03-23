b5631b25e2ea7454e702da4eb2a87e4d7451b45a
waal70 <myemail@myprovider.lol>
2026-03-23 18:22:54 +0100

    fix: add "-n" to refreshroles to prevent dependency warnings

--- refreshroles.sh
+++ refreshroles.sh
@@ -4,4 +4,4 @@
 /bin/bash ./deleteroles.sh
 
 # Then run command to retrieve
-ansible-galaxy install -g -f -r roles/requirements-local.yml
+ansible-galaxy install -g -f -n -r roles/requirements-local.yml
